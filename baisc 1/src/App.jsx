f60867b7245010be35fdf4fd7399e490c55a01de
prashxant <notxdead0@gmail.com>
2025-03-18 19:44:47 +0530

    count counter usestate

--- baisc 1/src/App.jsx
+++ baisc 1/src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 
 
 function App() {
 
   return (
     <>
-      
+    <b>Hi There </b>
+    <Counter></Counter>
+
+    </>
+  )
+}
+
+
+
+
+
+function Counter (){
+
+  const [count,setCount] = useState(0)
+
+
+    function increaseCounter(){
+        setCount(count + 1 )
+    }
+
+
+
+  return (
+    <>
+    <h1>{count}</h1>
+    <button onClick={increaseCounter}>Increase Counter</button>
     </>
   )
 }
