d789554048147311218cd786e419e6b9576400f3
prashxant <notxdead0@gmail.com>
2025-03-25 20:31:02 +0530

    useState

--- baisc 1/src/App.jsx
+++ baisc 1/src/App.jsx
@@ -4,38 +4,43 @@ import { useState } from "react";
 function App() {
 
   return (
-    <>
-    <b>Hi There </b>
-    <Counter></Counter>
-
-    </>
+    <div style={{display:"flex", justifyContent:"center" , marginTop:"20vh"}} >
+      
+      <Counter/>
+    </div>
   )
 }
-
-
 
 
 
 function Counter (){
 
-  const [count,setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
+    function increaseCount(){
+      return setCount(count + 1) 
+      }
+    function decreaseCount(){
+      return setCount(count - 1) 
+      }
+      function reset(){
+        return setCount( 0) 
+        }
+      
+return(
+  <div>
+    <h1>
+      {count}
+    </h1>
+    <button onClick={decreaseCount}>Decrease count</button>
+    <button style={ { margin:"1vw"}} onClick={reset}>Reset count</button>
+    <button onClick={increaseCount}>Increase count</button>
+    </div>
+)
 
-
-    function increaseCounter(){
-        setCount(count + 1 )
-    }
-
-function decreaseCounter(){
-  setCount(count - 1 )
 }
 
-  return (
-    <>
-    <h1>{count}</h1>
-    <button onClick={increaseCounter}>Increase Counter</button>
-    <button onClick={decreaseCounter}>Decrease Counter</button>
-    </>
-  )
-}
+
+
 
 export default App
