5abc3b146f796797f5b5a61d61f255d775268d86
prashxant <notxdead0@gmail.com>
2025-04-02 19:40:54 +0530

    understang error page

--- baisc 1/src/App.jsx
+++ baisc 1/src/App.jsx
@@ -1,29 +1,52 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route,Link,useNavigate } from 'react-router-dom'
+import { BrowserRouter,Routes,Route,Link,useNavigate, Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <div>
 
       <BrowserRouter>
+      
+        <Routes>
+          <Route path="/" element={<Landing/>} >
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/neet/class-12" element={<Class12></Class12>} />
+            <Route path="*" element={<ErrorPage/>} />
+          </Route>
+        </Routes>
+      
+      </BrowserRouter>
+    </div>
+  )
+
+  function Landing(){
+    return <div >
       <Link to="/">Allen </Link>
       |
       <Link to="/neet/class-11">Class 11 </Link>
       |
       <Link to="/neet/class-12"> Class12</Link>
-        <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/neet/class-12" element={<Class12></Class12>} />
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="*" element={<ErrorPage/>} />
-        </Routes>
-      </BrowserRouter>
+      <div style={{
+          display:"flex",
+          justifyContent:"center",
+          alignItems:"center",
+          height:"80vh"}
+        }>
+      <Outlet/>
+      </div>
+      Footer| Contact Us
     </div>
-  )
+  }
+
+
+
+
+
 
   function ErrorPage(){
-    return < div style={{paddingTop:"40vh"}}>
+    return < div style={{paddingTop:"10vh"}}>
     sorry page not found
+    <br /><br /><br />
     </div>
   }
 
