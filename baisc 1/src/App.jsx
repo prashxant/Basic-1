62f701f3317c9206e696f5005fe43694ad865f60
prashxant <notxdead0@gmail.com>
2025-04-02 19:16:15 +0530

    understang error page

--- baisc 1/src/App.jsx
+++ baisc 1/src/App.jsx
@@ -1,25 +1,48 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route,Link,useNavigate } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Card>
-        <div>
-          hi my name is prashant
-        </div>
-      </Card>
-      <Card />
+
+      <BrowserRouter>
+      <Link to="/">Allen </Link>
+      |
+      <Link to="/neet/class-11">Class 11 </Link>
+      |
+      <Link to="/neet/class-12"> Class12</Link>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/neet/class-12" element={<Class12></Class12>} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="*" element={<ErrorPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
+
+  function ErrorPage(){
+    return < div style={{paddingTop:"40vh"}}>
+    sorry page not found
+    </div>
+  }
+
+  function Class12(){
+    const navigate = useNavigate()
+
+    function goHome(){
+       navigate("/")
+    }
+    return(
+      <>
+      hi wellcom to class 12 
+       <button onClick={goHome}>
+        Go to Home 
+       </button>
+      </>
+    )
+
+  }
 }
-
-function Card({children}){
-  return <div>
-  {children}
-  </div>
-}
-
-
-
 
 export default App
