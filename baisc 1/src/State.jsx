3fb0e043b6b818a9b44f41533fda0f2373c4e0a0
prashxant <notxdead0@gmail.com>
2025-04-03 09:50:23 +0530

    rooling up the state ,prop drilling

--- baisc 1/src/State.jsx
+++ baisc 1/src/State.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function State() {
+    const [bulbOn,setBulbOn] = useState(true)
   return (
-    <div>State</div>
+    <div>
+
+        <BulbState bulbOn={bulbOn}/>
+        <ToggleBlub setBulbOn={setBulbOn}/>
+    </div>
   )
 }
+
+        function BulbState ({bulbOn}){
+            
+        return<div>
+            {bulbOn?"Bulb is on":"Bulb  off "}
+        </div>
+        }
+
+       
+
+        function ToggleBlub({setBulbOn}){
+            function Switch(){
+                setBulbOn(c => !c)
+            }
+            return(<div>
+                <button onClick={Switch} >Bulb Switch</button>
+            </div>
+            )}
+
 
 export default State
