1d8fa6d6a3644bd085ab772a73e1cf3de8b67c8d
prashxant <notxdead0@gmail.com>
2025-04-07 15:01:26 +0530

    understanding coustum hook

--- baisc 1/src/CustomHook.jsx
+++ baisc 1/src/CustomHook.jsx
@@ -1,31 +1,41 @@
 import React, { useState } from 'react'
 
-function useCounter(){
+function useCounter (){
+    const[count,setCount] = useState(0)
+    function handleClick(){
+    setCount(c => c+1)
+    }
 
-    const [count,setCount] = useState(0)
-
-function increaseCount(){
-    setCount(count + 1)
+    return {
+        count : count,
+        handleClick: handleClick
+    }
 }
-return {
-    count : count,
-    increaseCount : increaseCount
-}
-
-
-}
-
-
-
 
 function CustomHook() {
-    const {count,increaseCount} = useCounter()
-    
-  return (
-    <div>
-        <button onClick={increaseCount}>{count}</button>
-    </div>
+  
+
+    return (
+  
+<div>
+    <S/>
+    <S/>
+    <S/>
+    <S/>
+</div>
   )
 }
+
+
+function S (){
+const {count , handleClick}= useCounter()
+    return <div>
+        
+    <div>
+        <button onClick={handleClick}>{count}</button>
+    </div>
+    </div>
+}
+
 
 export default CustomHook
