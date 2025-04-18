cda54de93674f53def5626b6e9df73dc71c2bb88
prashxant <notxdead0@gmail.com>
2025-04-18 17:55:37 +0530

    typescript

---
+++ baisc 1/index.js
@@ -0,0 +1,21 @@
+"use strict";
+// let x: number = 4
+// console.log(x);
+// function greeting  (FirstName : string){
+//     console.log("Namaste " + FirstName)
+// }
+// greeting("hahaha") 
+// function Sum (b: number,a: number): number{
+// return a + b 
+// }
+// let o = Sum(5,7);
+// console.log(o)
+function isLeagal(age) {
+    if (age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isLeagal(42));
---
+++ baisc 1/index.ts
@@ -0,0 +1,24 @@
+
+// let x: number = 4
+// console.log(x);
+
+// function greeting  (FirstName : string){
+//     console.log("Namaste " + FirstName)
+// }
+// greeting("hahaha") 
+
+// function Sum (b: number,a: number): number{
+// return a + b 
+// }
+// let o = Sum(5,7);
+// console.log(o)
+
+
+function isLeagal ( age: number ) {
+    
+    if (age > 18)
+        {return true }
+    else
+        { return false}
+}
+console.log(isLeagal(42))
