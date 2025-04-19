65f8b8313ac96f3fbe1f1b096ddd2ce08a1672d1
prashxant <notxdead0@gmail.com>
2025-04-19 15:03:28 +0530

    llala

--- baisc 1/index.ts
+++ baisc 1/index.ts
@@ -1,35 +1,35 @@
 
-let x: number = 4
-console.log(x);
+// let x: number = 4
+// console.log(x);
 
-function greeting  (FirstName : string){
-    console.log("Namaste " + FirstName)
-}
-greeting("hahaha") 
+// function greeting  (FirstName : string){
+//     console.log("Namaste " + FirstName)
+// }
+// greeting("hahaha") 
 
-function Sum (b: number,a: number): number{
-return a + b 
-}
-let o = Sum(5,7);
-console.log(o)
-
-
-function isLeagal ( age: number ) {
-    if (age > 18)
-        {return true }
-    else
-        { return false}
-}
-console.log(isLeagal(42))
+// function Sum (b: number,a: number): number{
+// return a + b 
+// }
+// let o = Sum(5,7);
+// console.log(o)
 
 
+// function isLeagal ( age: number ) {
+//     if (age > 18)
+//         {return true }
+//     else
+//         { return false}
+// }
+// console.log(isLeagal(42))
 
-function delayed (fn: ()=>void){
 
-    setTimeout(fn,1000)
 
-}
+// function delayed (fn: ()=>void){
 
-delayed( function send (){
-    console.log("hello")
-})
+//     setTimeout(fn,1000)
+
+// }
+
+// delayed( function send (){
+//     console.log("hello")
+// })
