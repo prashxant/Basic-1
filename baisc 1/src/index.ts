452ac4e8b6497f54d005d934fdb4ac26f39f05a5
prashxant <notxdead0@gmail.com>
2025-04-22 20:35:26 +0530

    wow

--- baisc 1/src/index.ts
+++ baisc 1/src/index.ts
@@ -1,53 +1,53 @@
 
-// let x: number = 4
-// console.log(x);
+let x: number = 4
+console.log(x);
 
-// function greeting  (FirstName : string){
-//     console.log("Namaste " + FirstName)
-// }
-// greeting("hahaha") 
+function greeting  (FirstName : string){
+    console.log("Namaste " + FirstName)
+}
+greeting("hahaha") 
 
-// function Sum (b: number,a: number): number{
-// return a + b 
-// }
-// let o = Sum(5,7);
-// console.log(o)
-
-
-// function isLeagal ( age: number ) {
-//     if (age > 18)
-//         {return true }
-//     else
-//         { return false}
-// }
-// console.log(isLeagal(42))
+function Sum (b: number,a: number): number{
+return a + b 
+}
+let o = Sum(5,7);
+console.log(o)
 
 
-
-// function delayed (fn: ()=>void){
-
-//     setTimeout(fn,1000)
-
-// }
-
-// delayed( function send (){
-//     console.log("hello")
-// })
-
-// root dir out dir 
+function isLeagal ( age: number ) {
+    if (age > 18)
+        {return true }
+    else
+        { return false}
+}
+console.log(isLeagal(42))
 
 
-    // function greet  (user: {
-    //     name: string,
-    //     age: number
-    // }) {
-    // console.log("hi" + user.name)
-    // }
 
-    // greet({
-    //     name:"prashant",
-    //     age: 77
-    // })
+function delayed (fn: ()=>void){
+
+    setTimeout(fn,1000)
+
+}
+
+delayed( function send (){
+    console.log("hello")
+})
+
+root dir out dir 
+
+
+    function greet  (user: {
+        name: string,
+        age: number
+    }) {
+    console.log("hi" + user.name)
+    }
+
+    greet({
+        name:"prashant",
+        age: 77
+    })
     interface  Address {
      
             city: string,
@@ -98,3 +98,28 @@
             console.log("i am illlegal")
             }
       
+    interface People {
+        name: string ,
+        age: number ,
+        // greet : ()=> string , // input that return string as output //
+    }
+
+    let person:  People = {
+        name : "Prashant",
+        age  : 77,
+        greet ( )  {
+             return "hi  how you "  
+      }
+    }
+    
+    class Manager implements People {
+        name : string;
+        age : number;
+
+        constructor (name:string, age:number) {
+            this.name = name;
+            this.age = age;
+        }
+
+    }
+  
